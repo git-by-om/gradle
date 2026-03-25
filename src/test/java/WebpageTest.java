c9fa35bce6e847c492ba79ab8e5d335721eca4bb
git-by-om <omagarwalla22@gmail.com>
2026-03-25 22:55:49 +0530

    Adding index, styles, logo, files

---
+++ src/test/java/WebpageTest.java
@@ -0,0 +1,38 @@
+
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class WebpageTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://arc427.github.io/experiment3/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Your Page Title Here";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        assertTrue(true, "Title should contain your name");
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
+
